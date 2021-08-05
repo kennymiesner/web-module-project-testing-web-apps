ec935bcc538b0fb60041bd92db139cf2ab4b2157
Kenny Miesner <84544990+kennymiesner@users.noreply.github.com>
2021-08-05 16:39:17 -0500

    First three tests successfully running

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -5,15 +5,22 @@ import userEvent from '@testing-library/user-event';
 import ContactForm from './ContactForm';
 
 test('renders without errors', ()=>{
-    
+  render(<ContactForm />)
 });
 
 test('renders the contact form header', ()=> {
-    
+  render(<ContactForm />)
+  const header = screen.queryByText(/contact form/i)    
+  expect(header).toBeInTheDocument()
+  console.log(header)
 });
 
 test('renders ONE error message if user enters less then 5 characters into firstname.', async () => {
-    
+  render(<ContactForm />)
+  const firstNameInput = screen.getByLabelText(/First Name/i)
+  userEvent.type(firstNameInput, 'ABC')
+  const firstNameError = screen.getByTestId('error')
+  expect(firstNameError).toBeInTheDocument()
 });
 
 test('renders THREE error messages if user enters no values into any fields.', async () => {
